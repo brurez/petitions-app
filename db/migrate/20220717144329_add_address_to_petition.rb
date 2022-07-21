665dd776763fea2ff84bdb7593339791b5b02869
Bruno de Rezende <brunoderezende@hotmail.com>
2022-07-21 08:10:09 -0300

    Changes postal_code type

--- db/migrate/20220717144329_add_address_to_petition.rb
+++ db/migrate/20220717144329_add_address_to_petition.rb
@@ -4,6 +4,6 @@ class AddAddressToPetition < ActiveRecord::Migration[7.0]
     add_column :petitions, :city, :string, null: false
     add_column :petitions, :state, :string, limit: 2
     add_column :petitions, :country, :string, null: false, limit: 2
-    add_column :petitions, :postal_code, :integer
+    add_column :petitions, :postal_code, :string
   end
 end
--- spec/factories/petitions.rb
+++ spec/factories/petitions.rb
@@ -2,5 +2,12 @@ FactoryBot.define do
   factory :petition do
     title { Faker::Lorem.sentence }
     description { Faker::Lorem.paragraph }
+    latitude { 1 }
+    longitude { 2 }
+    address { "8 Lewisham Way" }
+    city { "London" }
+    state { "LD" }
+    country { "UK" }
+    postal_code { "SE14 6NW" }
   end
 end
