636774131a97d343419a8a5496e04308d6c434ae
Bruno de Rezende <brunoderezende@hotmail.com>
2022-07-23 17:57:37 -0300

    Fix host config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://final-project-brurez.vercel.app'
+    origins 'final-project-brurez.vercel.app'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 
   allow do
-    origins 'http://localhost:3000'
+    origins 'localhost:3000'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
