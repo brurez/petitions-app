3d98a71ba14e93115a250241a8fc5b2617ca3fc2
Bruno de Rezende <brunoderezende@hotmail.com>
2022-07-23 22:45:20 -0300

    Allows any server

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'final-project-brurez.vercel.app'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-
-  allow do
-    origins 'localhost:3000'
+    origins '*'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
