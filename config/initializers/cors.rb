6cdd5da58b3daf9ec98633e7c44d2e3997f2b13b
Bruno de Rezende <brunoderezende@hotmail.com>
2022-07-27 09:28:27 -0300

    Changes cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins %w[http://localhost:3000 https://final-project-two-opal.vercel.app]
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
