7124ea735abb37e94eb2be37c41231ec4fbd16ec
Bruno de Rezende <brunoderezende@hotmail.com>
2022-07-30 10:13:14 -0300

    Cors again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,16 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'final-project-brurez.vercel.app'
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
+    origins 'final-project-two-opal.vercel.app'
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
+    origins 'localhost:3000'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
